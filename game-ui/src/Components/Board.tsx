550433cd89b0851ccb6c3e44a86b6be73777e3cf
youne <younes.vg20@gmail.com>
2023-02-14 12:33:30 +0100

    finish the game-ui

--- game-ui/src/Components/Board.tsx
+++ game-ui/src/Components/Board.tsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-export const Board = () => {
+type BoardProps = {
+    board: number[];
+    setGameState: (status: string) => void;
+    setWinner: (status: string) => void;
 
+}
+export const Board = ({board, setGameState, setWinner}: BoardProps) => {
+
+    const [player, setPLayer] = useState('X');
+    const [cells, setCells] = useState(Array(9).fill(''));
+
+    const playingGame = (value: number) => {
+        if (cells[value] !== '') {
+            return;
+        }
+
+        let squares = [...cells];
+
+        if (player === 'X') {
+
+            squares[value] = 'X';
+            const payload = {
+                index: value,
+                player: 'X'
+            };
+            axios.post('http://localhost:3002/tictactoe/playing', payload).then(({data}) => {
+                setPLayer('O');
+                if(data.status === 'FINISHED') {
+                    setGameState(data.status);
+                    setWinner(data.winner);
+                }
+            });
+        }
+
+        if (player === 'O') {
+            squares[value] = 'O';
+            const payload = {
+                index: value,
+                player: 'O'
+            };
+            axios.post('http://localhost:3002/tictactoe/playing', payload).then(({data}) => {
+                setPLayer('X');
+                if(data.status === 'FINISHED') {
+                    setGameState(data.status);
+                    setWinner(data.winner)
+                }
+            });
+
+        }
+
+        setCells(squares);
+    }
 
     return (
         <>
+            <div className="board">
+                {
+                    board && board.map((value) => {
+                        return (
+                            <button key={value} className="cell" onClick={() => playingGame(value)}>{cells[value]}</button>)
+                    })
+                }
+            </div>
         </>
     )
 }
--- game-ui/src/Views/Game.tsx
+++ game-ui/src/Views/Game.tsx
@@ -4,15 +4,29 @@ import {Welcome} from "../Components/Welcome";
 import {Board} from "../Components/Board";
 import {Button} from "react-bootstrap";
 
-export const Game = () => {
-    //const squareIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const [gameState, setGameState] = useState<string>('')
-   // const [winner, setWinner] = useState<string>('');
 
+export const Game = () => {
+
+    const squareIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const [gameState, setGameState] = useState<string>('')
+    const [winner, setWinner] = useState<string>('');
 
     return (
         <Fragment>
             {!['NEW', 'IN_PROGRESS', 'FINISHED'].includes(gameState) && <Welcome setGameState={setGameState}/>}
+
+            {gameState === 'NEW' &&
+                <Board board={squareIndexes} setGameState={setGameState} setWinner={setWinner}/>}
+
+            {gameState === 'FINISHED' &&
+                <div className="d-flex justify-content-center">
+                    <div className="title">
+                        {(winner && winner.length > 0) ? (<h3>Winner : {winner}</h3>) : (<h3>Draw. No winner</h3>)}
+                        <Button variant="success" className="center-screen" onClick={() => setGameState('')}>Go Home
+                            Page</Button>
+                    </div>
+                </div>}
+
         </Fragment>
     )
 }
